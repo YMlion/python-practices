d5c3a555502e3a27d0e6976f7b9b4330eaae7c95
YMlion <lm_yao@163.com>
2018-01-09 14:25:40 +0800

    user

--- websocket_tornado.py
+++ websocket_tornado.py
@@ -1,18 +1,33 @@
 """use tornado to implement websocket"""
+import json
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
-import json
 
 
-class MsgHandler(tornado.websocket.WebSocketHandler):
+class User():
+
+    def __init__(self, token):
+        self.token = token
+        self._room_id = None
+
+    @property
+    def room_id(self):
+        return self._room_id
+
+    @room_id.setter
+    def room_id(self, value):
+        self._room_id = value
+
+
+class MsgHandler(tornado.websocket.WebSocketHandler):   
+    users = []
 
     def open(self):
         print("WebSocket opened")
 
     def on_message(self, message):
         print('recieved data from client is ' + message)
-        # self.write_message(u"You said: " + message)
         dict_data = json.loads(message)
         action = dict_data['action']
         if action == 'exit':
@@ -20,15 +35,16 @@ class MsgHandler(tornado.websocket.WebSocketHandler):
         elif action == 'token':
             token = dict_data['data']
             print('the user token is ' + token)
-            if token == None:
+            if not token or token == '':
                 self.write_error(101)
                 self.close()
+            else:
+                MsgHandler.users.append(User(token))
         elif action == 'start':
             game = dict_data['data']
             if game == 'draw_guess':
                 result = {'code': 250, 'action': 'start', 'result': 55465}
                 self.write_message(json.dumps(result))
-            
 
     def on_close(self):
         print("WebSocket closed")
